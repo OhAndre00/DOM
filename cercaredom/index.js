82c62eb3b158abd6da69841774d21a910defc3c9
Andrea <andrea.seidita00@gmail.com>
2025-01-14 09:32:38 +0100

    esercizio 2 svolto

---
+++ cercaredom/index.js
@@ -0,0 +1,17 @@
+const $body = document.querySelector("body");
+
+const $elementiParagrafo = document.querySelectorAll(".paragrafo");
+
+$elementiParagrafo.forEach((elemento) => {
+  console.log("p:", elemento.innerText);
+});
+
+const $elemento3 = document.getElementById("elemento-3");
+
+console.log($elemento3.innerText);
+
+const $elementiH2 = document.querySelectorAll("h2");
+
+$elementiH2.forEach((elemento) => {
+  console.log("h2:", elemento.innerText);
+});
